e5262ab9338c5417b9fe3bb64ada39132e585d09
Nguyễn Kiều Anh <kieuanh.dev@gmail.com>
2025-07-08 20:27:52 +0700

    fake db use factory & seeder

--- database/migrations/2025_07_08_124504_create_reviews_table.php
+++ database/migrations/2025_07_08_124504_create_reviews_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('reviews', function (Blueprint $table) {
             $table->id();
-            $table->string('review');
+            $table->text('review');
             $table->unsignedTinyInteger('rating');
             $table->timestamps();
             $table->foreignId('book_id')->constrained()->cascadeOnDelete();
