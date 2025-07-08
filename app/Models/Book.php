f11460ae20b7d1c9c40717781c434317281145c4
Nguyễn Kiều Anh <kieuanh.dev@gmail.com>
2025-07-08 21:31:03 +0700

    fix factory and use local scope

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -13,4 +14,9 @@ class Book extends Model
     public function reviews(){
         return $this->hasMany(Review::class);
     }
+
+    public function scopeTitle(Builder $query, ?string $title){
+        return $query->where("title","like","%".$title."%");
+    }
+
 }
